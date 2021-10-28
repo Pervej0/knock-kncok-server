39839bf62486d18cc7024dbfec7eceac584ec344
Pervej0 <mdparvez222khan@gmail.com>
2021-10-29 00:37:19 +0600

    Server is connecte with DB and checked

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// databse details-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjbgh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(uri);
+
+const run = async () => {
+  try {
+    await client.connect();
+    console.log("DB is connected");
+  } finally {
+    // client.close();
+  }
+};
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("WELCOME TO SERVER WORLD");
 });
