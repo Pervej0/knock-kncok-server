d78143acda4ec4f5dfec80968ea8a6740a06473a
Pervej0 <mdparvez222khan@gmail.com>
2021-10-29 00:28:51 +0600

    index and dotenv file setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
