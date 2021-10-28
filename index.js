36d61a8c95a093f711778910ebe9536142399402
Pervej0 <mdparvez222khan@gmail.com>
2021-10-29 00:31:20 +0600

    Server started

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("WELCOME TO SERVER WORLD");
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port: ", port);
+});
