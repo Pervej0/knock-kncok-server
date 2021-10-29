a7f9945b73d2bc380d577c1189cecfc63d663ae1
Pervej0 <mdparvez222khan@gmail.com>
2021-10-30 02:59:29 +0600

    myOrder & manageOrders page required db method added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,9 +21,65 @@ const run = async () => {
   try {
     await client.connect();
     console.log("DB is connected");
+    const database = client.db("knock_knock_DB");
+    const servicesCollection = database.collection("knock_knock_Services");
+    const orderCollection = database.collection("order_collection");
+    // get all products
+    app.get("/products", async (req, res) => {
+      const query = await servicesCollection.find({}).toArray();
+      res.send(query);
+    });
 
-    app.get("/services", (req, res) => {
-      res.send("SERVICES FILES IS OKEY GOOD TO GO");
+    // get speacific product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
+
+    // inseert order details
+    app.post("/orders", async (req, res) => {
+      const doc = req.body;
+      const result = await orderCollection.insertOne(doc);
+      res.send(result);
+    });
+
+    // get ordered product
+    app.get("/myOrders/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await orderCollection.find({ email: email }).toArray();
+      res.send(result);
+      // console.log("helllllowin", email);
+    });
+
+    // delete specific order
+    app.delete("/myOrders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+      console.log(result);
+    });
+
+    // get all orders-
+    app.get("/myOrders", async (req, res) => {
+      const query = await orderCollection.find({}).toArray();
+      res.send(query);
+    });
+
+    // update order status
+    app.put("/myOrders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: "approved",
+        },
+      };
+      const result = await orderCollection.updateOne(filter, updateDoc);
+      res.send(result);
+      console.log(result);
     });
   } finally {
     // client.close();
