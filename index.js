ffd2a8246f9304aaa4b574ad5ea8cf51f8c8668b
pervej0 <mdparvez222khan@gmail.com>
2023-05-08 12:19:39 +0600

    DB access password changed

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 // databse details-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjbgh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjbgh.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = "mongodb+srv://<username>:<password>@cluster0.sjbgh.mongodb.net/?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
